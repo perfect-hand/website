3e83423b040aed94dda1174ec4a039fd82c8583d
Nick Pruehs <dev@npruehs.de>
2023-02-15 02:09:28 +0100

    Add Twitter bootstrap.

--- src/main.js
+++ src/main.js
@@ -1,8 +1,9 @@
 import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router.js";
+import * as bootstrap from 'bootstrap'
 
-import "./assets/main.css";
+import "./assets/main.scss";
 
 const app = createApp(App);
 
